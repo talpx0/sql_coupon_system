d11644aa005a13adb345d16e48b7b27727720fad
talpx0 <talpx.work@gmail.com>
2023-09-21 15:49:18 -0400

    add svg diagram

--- promo.sql
+++ promo.sql
@@ -1,36 +1,44 @@
+CREATE TABLE "users" (
+  "id" bigserial PRIMARY KEY 
+);
+
+CREATE TABLE "product"(
+  "id" bigserial PRIMARY KEY
+);
+
 CREATE TABLE "promotion" (
   "id" bigserial PRIMARY KEY,
   "name" varchar,
   "description" text,
   "start_date" timestamptz,
   "end_date" timestamptz, 
-  "total_user" bigint
+  "total_user" bigserial
 ); 
 
 CREATE TABLE "user_promotion_usage"(
   "id" bigserial PRIMARY KEY,
-  "user_id" bigint REFERENCES "users"("id"),
-  "promotion_id" bigint REFERENCES "promotion"("id"),
-  "per_total_counts" bigint 
+  "user_id" bigserial REFERENCES "users"("id"),
+  "promotion_id" bigserial REFERENCES "promotion"("id"),
+  "per_total_counts" bigserial 
 );
 
 CREATE TABLE "user_coupon_usage"(
   "id" bigserial PRIMARY KEY,
-  "user_id" bigint REFERENCES "users"("id"),
-  "dis_id" bigint,
+  "user_id" bigserial REFERENCES "users"("id"),
+  "dis_id" bigserial,
   "use_count" int   
 );
 
 CREATE TABLE "user_coupon_usage2"(
   "id" bigserial PRIMARY KEY,
-  "user_id" bigint REFERENCES "users"("id"),
-  "dis_id" bigint,
+  "user_id" bigserial REFERENCES "users"("id"),
+  "dis_id" bigserial,
   "use_at" timestamptz
 )
 CREATE TABLE "percentage_dis" (
   "id" bigserial PRIMARY KEY,
+  "promotion_id" bigserial,
   "coupon_code" varchar(100),
-  "promotion_id" bigint REFERENCES "promotion"("id"),
   "percentage_off" varchar(10) NOT NULL,
   "start_date" timestamp NOT NULL,
   "end_date" timestamp NOT NULL,
@@ -39,8 +47,8 @@ CREATE TABLE "percentage_dis" (
 
 CREATE TABLE "fixed_amount_dis" (
   "id" bigserial PRIMARY KEY,
+  "promotion_id" bigserial,
   "coupon_code" varchar(100),
-  "promotion_id" bigint REFERENCES "promotion"("id"),
   "fixed_amount_off" varchar(10) NOT NULL,
   "start_date" timestamp NOT NULL,
   "end_date" timestamp NOT NULL,
@@ -49,7 +57,7 @@ CREATE TABLE "fixed_amount_dis" (
 
 CREATE TABLE "bogo_dis" (
   "id" bigserial PRIMARY KEY,
-  "promotion_id" bigint REFERENCES "promotion"("id"),
+  "promotion_id" bigserial,
   "coupon_code" varchar(100),
   "buy_quantity" int NOT NULL,
   "free_quantity" int NOT NULL,
@@ -60,7 +68,7 @@ CREATE TABLE "bogo_dis" (
 
 CREATE TABLE "free_shipping_dis" (
   "id" bigserial PRIMARY KEY,
-  "promotion_id" bigint REFERENCES "promotion"("id"),
+  "promotion_id" bigserial,
   "coupon_code" varchar(100),
   "start_date" timestamp NOT NULL,
   "end_date" timestamp NOT NULL,
@@ -69,9 +77,9 @@ CREATE TABLE "free_shipping_dis" (
 
 CREATE TABLE "bundled_dis" (
   "id" bigserial PRIMARY KEY,
-  "promotion_id" bigint REFERENCES "promotion"("id"),
+  "promotion_id" bigserial,
   "coupon_code" varchar(100),
-  "bundle_product_id" bigint NOT NULL,
+  "bundle_product_id" bigserial NOT NULL,
   "bundle_price" varchar(12) NOT NULL,
   "start_date" timestamp NOT NULL,
   "end_date" timestamp NOT NULL
@@ -79,7 +87,7 @@ CREATE TABLE "bundled_dis" (
 
 CREATE TABLE "volume_dis" (
   "id" bigserial PRIMARY KEY,
-  "promotion_id" bigint REFERENCES "promotion"("id"),
+  "promotion_id" bigserial,
   "coupon_code" varchar(100),
   "minimum_quantity" int NOT NULL,
   "dis_percentage" varchar(10) NOT NULL,
@@ -88,69 +96,13 @@ CREATE TABLE "volume_dis" (
 );
 
 CREATE TABLE "promotion_config" (
-  "product_id" bigint UNIQUE NOT NULL,
-  "promotion_id" bigint NOT NULL REFERENCES "promotion"("id"),
-  "percentage_dis_id" bigint REFERENCES "percentage_dis"("id"),
-  "fixed_amount_dis_id" bigint REFERENCES "fixed_amount_dis"("id"),
-  "bogo_dis_id" bigint REFERENCES "bogo_dis"("id"),
-  "free_shipping_dis_id" bigint REFERENCES "free_shipping_dis"("id"),
-  "bundled_dis_id" bigint REFERENCES "bundled_dis"("id"),
-  "volume_dis_id" bigint REFERENCES "volume_dis"("id")
+  "product_id" bigserial NOT NULL REFERENCES "product"("id"),
+  "promotion_id" bigserial NOT NULL REFERENCES "promotion"("id"),
+  "percentage_dis_id" bigserial REFERENCES "percentage_dis"("id"),
+  "fixed_amount_dis_id" bigserial REFERENCES "fixed_amount_dis"("id"),
+  "bogo_dis_id" bigserial REFERENCES "bogo_dis"("id"),
+  "free_shipping_dis_id" bigserial REFERENCES "free_shipping_dis"("id"),
+  "bundled_dis_id" bigserial REFERENCES "bundled_dis"("id"),
+  "volume_dis_id" bigserial REFERENCES "volume_dis"("id")
 );
 
-
-
-CREATE INDEX ON "promotion" ("name");
-
-CREATE INDEX ON "promotion" ("start_date");
-
-CREATE INDEX ON "promotion" ("end_date");
-
-CREATE INDEX ON "percentage_dis" ("percentage_off");
-
-CREATE INDEX ON "percentage_dis" ("promotion_id");
-
-CREATE INDEX ON "fixed_amount_dis" ("promotion_id");
-
-CREATE INDEX ON "bogo_dis" ("promotion_id");
-
-CREATE INDEX ON "free_shipping_dis" ("promotion_id");
-
-CREATE INDEX ON "bundled_dis" ("bundle_product_id");
-
-CREATE INDEX ON "volume_dis" ("promotion_id");
-
-CREATE INDEX ON "promotion_config" ("product_id");
-
-CREATE INDEX ON "promotion_config" ("promotion_id");
-
-CREATE INDEX ON "promotion_config" ("percentage_dis_id");
-
-CREATE INDEX ON "promotion_config" ("fixed_amount_dis_id");
-
-CREATE INDEX ON "promotion_config" ("bogo_dis_id");
-
-CREATE INDEX ON "promotion_config" ("free_shipping_dis_id");
-
-CREATE INDEX ON "promotion_config" ("bundled_dis_id");
-
-CREATE INDEX ON "promotion_config" ("volume_dis_id");
-
-
-ALTER TABLE "percentage_dis" ADD FOREIGN KEY ("promotion_id") REFERENCES "promotion" ("id");
-
-ALTER TABLE "fixed_amount_dis" ADD FOREIGN KEY ("promotion_id") REFERENCES "promotion" ("id");
-
-ALTER TABLE "bogo_dis" ADD FOREIGN KEY ("promotion_id") REFERENCES "promotion" ("id");
-
-ALTER TABLE "free_shipping_dis" ADD FOREIGN KEY ("promotion_id") REFERENCES "promotion" ("id");
-
-ALTER TABLE "bundled_dis" ADD FOREIGN KEY ("promotion_id") REFERENCES "promotion" ("id");
-
-ALTER TABLE "bundled_dis" ADD FOREIGN KEY ("bundle_product_id") REFERENCES "product" ("id");
-
-ALTER TABLE "volume_dis" ADD FOREIGN KEY ("promotion_id") REFERENCES "promotion" ("id");
-
-ALTER TABLE "promotion_config" ADD FOREIGN KEY ("product_id") REFERENCES "product" ("id");
-
-ALTER TABLE "promotion_config" ADD FOREIGN KEY ("promotion_id") REFERENCES "promotion" ("id");
